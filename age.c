ccc62230c7aa15ba287e93cfc87dd74905f3ee1f
jigyasa-singhal <155255645+jigyasa-singhal@users.noreply.github.com>
2024-04-09 12:48:24 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+  int age = 22; 
+  printf("age is %d",age,"\n");
+  printf("age is %d",age,"\n");
+  printf("age is lion %d",age,"\n");
+  printf(" age is %d", age);
+  // printf("age is %d, age, /n");
+
+  return 0;
+} 
